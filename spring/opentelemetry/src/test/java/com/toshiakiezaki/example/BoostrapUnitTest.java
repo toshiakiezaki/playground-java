faf9f81eb3ad7d785e6b0f5150065cf7bf84c36c
Toshiaki Ezaki <toshiaki.ezaki@gmail.com>
2023-10-26 14:05:58 -0300

    Added bootstrap class for Spring

---
+++ spring/opentelemetry/src/test/java/com/toshiakiezaki/example/BoostrapUnitTest.java
@@ -0,0 +1,13 @@
+package com.toshiakiezaki.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoostrapUnitTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
