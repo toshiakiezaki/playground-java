36564cc9ef755bc35e243e595b7df9132caef36b
Toshiaki Ezaki <toshiaki.ezaki@gmail.com>
2023-10-27 18:32:52 -0300

    Added basic structure for native compiling mode

---
+++ quarkus/native/src/test/java/com/toshiakiezaki/example/GreetingResourceIntegrationTest.java
@@ -0,0 +1,8 @@
+package com.toshiakiezaki.example;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIntegrationTest extends GreetingResourceUnitTest {
+    // Execute the same tests but in packaged mode.
+}
