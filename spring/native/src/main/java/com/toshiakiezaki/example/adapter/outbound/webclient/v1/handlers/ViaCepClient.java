b9af95be33ad6efe28a0992410d85dbf3932cbb7
Toshiaki Ezaki <toshiaki.ezaki@gmail.com>
2025-09-04 23:19:35 -0300

    Registered ViaCep entity class for reflection for binding

--- spring/native/src/main/java/com/toshiakiezaki/example/adapter/outbound/webclient/v1/handlers/ViaCepClient.java
+++ spring/native/src/main/java/com/toshiakiezaki/example/adapter/outbound/webclient/v1/handlers/ViaCepClient.java
@@ -2,12 +2,15 @@ package com.toshiakiezaki.example.adapter.outbound.webclient.v1.handlers;
 
 import com.toshiakiezaki.example.adapter.outbound.webclient.v1.entities.ViaCepData;
 import com.toshiakiezaki.example.domain.entities.PostalCode;
+
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Repository
+@RegisterReflectionForBinding(ViaCepData.class)
 public class ViaCepClient {
 
 	private static final String DEFAULT_TYPE = "json";
