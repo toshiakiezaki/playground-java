b4ca647121b43c57c447623a72cd639686e865a4
Toshiaki Ezaki <toshiaki.ezaki@gmail.com>
2025-09-08 02:27:47 -0300

    Added data samples for postal codes

---
+++ spring/native/sample.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+curl -si http://localhost:8080/v1/postal-codes/01001000 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89214000 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89214640 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89237100 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89216240 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89204640 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89221585 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89225570 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89204643 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89210618 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89214260 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89201215 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89204040 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89204041 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89204635 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89216001 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/89216000 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/83325110 && echo && echo && echo
+curl -si http://localhost:8080/v1/postal-codes/83326185 && echo && echo && echo
